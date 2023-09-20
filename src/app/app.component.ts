b5febf5fcb55a2e5dba595c081636ce11b8b6eca
Manu-shree <17manu1998@gmail.com>
2023-09-20 19:17:18 +0530

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'resume';
+  sendEmail() {
+    console.log('clicking');
+    
+    window.location.href = "mailto:17manu1998@gmail.com";
+  }
+  sendEmail1(name: string, email: string, subject: string, message: string) {
+    const recipientEmail = 'recipient@example.com';
+    const mailtoUrl = `mailto:${recipientEmail}?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(`Name: ${name}\nEmail: ${email}\n\n${message}`)}`;
+    window.open(mailtoUrl);
+  }
+}
+
